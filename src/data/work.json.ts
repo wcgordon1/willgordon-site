136e21130d1505020c222bc1fdc2f533e92370fa
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-01-01 22:37:50 +0200

    cleaneed up -  remove astrolib added metas

--- src/data/work.json.ts
+++ src/data/work.json.ts
@@ -17,13 +17,13 @@ const workTwo: Template = {
   title: "Lexington Themes",
 };
 const workThree: Template = {
-  url: "https://unwrapped.design/figmax/",
+  url: "https://www.figma.com/community/file/1284100224206308057/figmax90",
   description: "Old School covers for your Figma projects",
   avatar: "/figmax.svg",
   title: "FigMax90",
 };
 const workfour: Template = {
-  url: "https://unwrapped.design/onda/",
+  url: "https://www.figma.com/community/file/1254393856683700911/onda",
   description: "A set of wave wallpapers in 5k and mobile resolution to use on your design projects",
   avatar: "/onda.svg",
   title: "Onda",

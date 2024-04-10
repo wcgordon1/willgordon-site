6508817758df3b52a67a65333f911d9efbfa3a72
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-04-10 23:30:31 +0300

    new pages, redesigned profoliox

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,55 @@
 import { defineCollection, z } from "astro:content";
-
+const work = defineCollection({
+  schema: z.object({
+    pubDate: z.date(),
+    title: z.string(),
+    subtitle: z.string(),
+    live: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+  }),
+});
+const store = defineCollection({
+  schema: z.object({
+    price: z.string(),
+    title: z.string(),
+    preview: z.string(),
+    checkout: z.string(),
+    license: z.string(),
+    highlights: z.array(z.string()),
+    description: z.string(),
+    features: z.array(
+      z.object({
+        title: z.string(),
+        description: z.string(),
+      })
+    ),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    images: z.array(
+      z.object({
+        url: z.string(),
+        alt: z.string(),
+      })
+    ),
+  }),
+});
+const projects = defineCollection({
+  schema: z.object({
+    pubDate: z.date(),
+    title: z.string(),
+    subtitle: z.string(),
+    live: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+  }),
+});
 const postsCollection = defineCollection({
     schema: z.object({
       title: z.string(),
@@ -15,5 +65,8 @@ const postsCollection = defineCollection({
  });
 
 export const collections = {
+  work: work,
+  store: store,
+  projects: projects,
   posts: postsCollection,
 };
